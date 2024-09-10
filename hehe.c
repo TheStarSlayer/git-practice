d5d7588b3279191fdbe0000e616c940f6f64e30e
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-09-10 18:15:29 +0530

    Removed hehe.c and hello.c

--- hehe.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Bondman!!, Anya-san is in dangerrr!!\n");
-	printf("Huhhhh!!, from what, Yor-san...\n");
-	printf("Taskehte, bondmannnn...");
-	
-	return 0;
-}
--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Hello Git world\n");
-	printf("Hello branch boiii\n");
-	printf("This is now part of Github...\n");
-	return 0;
-}
